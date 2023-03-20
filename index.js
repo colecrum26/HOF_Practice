1ff48550bcff4175d7b33075825ab73592db477c
colecrum26 <cwcrumrine@gmail.com>
2023-03-20 10:26:05 -0500

    pushing mods

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 let nums = [1, 2, 3, 4, 5];
 let arr1 = [20, 40, 60, 80, 3];
 
-nums.reduce((sum, num) => {
-    return sum + num;
-}, 0);
+const pokemons = [
+    {
+        name: "pikachu",
+        type: "lightning",
+        damage: 20,
+    },  
+    {
+        name: "machamp",
+        type: "fighting",
+        damage: 60,
+    },  
+    {
+        name: "mew",
+        type: "psychic",
+        damage: 30,
+    },  
+    {
+        name: "charizard",
+        type: "fire",
+        damage: 100,
+    }
+];
+
+pokemons.forEach((pokemon)=>{
+    console.log(pokemon.name, pokemon.damage);
+})
+
+// nums.reduce((sum, num) => {
+//     return sum + num;
+// }, 0);
 
 // console.log(arr1.reduce((sum, num) => {
     // if(num % 10 !==0){
@@ -16,17 +43,17 @@ nums.reduce((sum, num) => {
 // },0)
 // );
 
-let sum = 0;
-arr1.forEach((num) => {
-    if(num % 10 !==0){
-        console.log("in the if" + num);
-    } else {
-        // console.log(num);
-        sum += num;
-    }
-    console.log(sum);
-    return sum;
-})
+// let sum = 0;
+// arr1.forEach((num) => {
+//     if(num % 10 !==0){
+//         console.log("in the if" + num);
+//     } else {
+//         // console.log(num);
+//         sum += num;
+//     }
+//     console.log(sum);
+//     return sum;
+// })
 
 // arr1.reduce(function (previousValue, currentValue, currentIndex, array){
 //     previousValue += currentValue;
